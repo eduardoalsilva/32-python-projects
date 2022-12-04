fd9c860c0e5cf57282b6a4d3edda019028753c43
Eduardo Silva <eduardoalsilva45@gmail.com>
2022-12-04 18:36:24 -0300

    Inserindo o arquivo madlibs.py

---
+++ 01-madlibs/madlibs.py
@@ -0,0 +1,8 @@
+adjective = input("Adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adjective}! It makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+
+print(madlib)
